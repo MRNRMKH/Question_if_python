675091245d41fae685072ab04c0fb95f34e15a00
Navid reza <navidrezameysami@gmail.com>
2024-07-31 22:51:42 -0700

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,17 @@
+my_num_one=int(input('enter  first number : '))
+my_num_tow=int(input('enter second number : '))
+
+sum= my_num_one + my_num_tow
+
+if sum<10: 
+
+    print("Not enough score")
+
+else:
+
+        if 20>= sum >18:
+                print(20)
+        if 18>= sum >15:
+                print(18)
+        if 15>= sum >=10:
+                print(15)
