b4e4d062a3c37c5128df2454acaf03d11a23c355
Navid reza <navidrezameysami@gmail.com>
2024-07-31 22:50:49 -0700

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,9 @@
+my_str = input(" enter your chracter : ")
+
+my_str_len = len(my_str)
+print(my_str_len)
+
+if my_str_len % 2 == 0:
+    print('length is even! ')
+else:
+    print('length is odd! ')
