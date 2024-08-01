730d9b42880a89b29a2ca8cb2efce392be06ab93
Navid reza <navidrezameysami@gmail.com>
2024-07-31 22:52:39 -0700

    Add files via upload

---
+++ Q10.py
@@ -0,0 +1,10 @@
+input_num = int(input('enter your namber : '))
+
+if input_num % 15 == 0:
+    print('15')
+elif input_num % 3 == 0:
+    print('3')
+elif input_num % 5 == 0:
+    print('5')
+else:
+    print('no')
